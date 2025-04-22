a1b76ae3a59d6c8e518e592d2f7907ac01ca0bf7
User <OrengeGame39@gmail.com>
2025-04-22 14:25:00 +0500

    первое сохранение

---
+++ app/src/main/java/com/example/skyfish/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.skyfish
+
+import android.content.SharedPreferences
+import android.graphics.Canvas
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private var score: Long = 0
+    var inc: Long = 1
+
+    private lateinit var background: View
+    private lateinit var fish: View
+    private lateinit var text_score: TextView
+
+    private lateinit var shared: SharedPreferences
+    private lateinit var sharedEditor: SharedPreferences.Editor
+    val canvas = Canvas()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        shared = getSharedPreferences("main", 0)
+        sharedEditor = shared.edit()
+        load()
+
+        background = findViewById(R.id.background)
+        fish = findViewById(R.id.fish)
+        text_score = findViewById(R.id.text_score)
+
+        background.draw(canvas)
+    }
+
+    private fun save(){
+        sharedEditor.putLong("score", score)
+        sharedEditor.putLong("inc", inc)
+        sharedEditor.commit()
+    }
+
+    private fun load(){
+        score = shared.getLong("score", 0)
+        inc = shared.getLong("inc", 1)
+    }
+}
